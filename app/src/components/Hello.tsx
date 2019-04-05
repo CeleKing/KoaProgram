0394a60ad9bdcdff4a7e428dc27216c14f579a17
celeking <celekingm_cn@outlook.com>
2019-04-05 23:39:30 +0800

    add koa-route

--- app/src/components/Hello.tsx
+++ app/src/components/Hello.tsx
@@ -1,18 +1,30 @@
 import * as React from 'react';
+import axios from 'axios'
 
 export interface HelloProps {
     compiler: string, 
     framework: string
 }
 
-// export const Hello = (props: HelloProps) => {
-//     <h1>Hello from {props.compiler} and {props.framework}!</h1>
-// }
-
 export default class Hello extends React.Component<HelloProps, {}> {
+    onClick() {
+        axios.get('/')
+            .then(function(res) {
+                console.log(res);
+            })
+            .catch(function(err) {
+                console.log(err);
+            })
+
+    }
     render() {
         const { compiler, framework} = this.props;
-        return <h1>Hello from {compiler} and {framework}!</h1>
+        return (
+            <div>
+                <h1>Hello from {compiler} and {framework}!</h1>
+                <button onClick={this.onClick.bind(this)}>Get Request</button>
+            </div>
+        )
     }
 }
 
--- app/src/server.js
+++ app/src/server.js
@@ -1,8 +1,19 @@
 const Koa = require('koa');
+var Router = require('koa-router');
+
 const app = new Koa()
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+    console.log(11);
+    console.log(ctx)
+})
 
 app.use(async ctx => {
     ctx.body = 'Hello word'
 })
 
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 app.listen(3000)
