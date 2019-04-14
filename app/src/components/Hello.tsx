af8d202aa673807daa5420a8369752fd05f27401
celeking <celekingm_cn@outlook.com>
2019-04-14 15:20:04 +0800

    fix props

--- app/src/components/Hello.tsx
+++ app/src/components/Hello.tsx
@@ -1,55 +1,47 @@
 import * as React from 'react';
 import {login, logout} from '../actions'
 import { StoreState} from '../types'
-import axios from 'axios'
-import { store } from '../store';
 import {Dispatch} from 'redux';
 import { connect } from 'react-redux';
 
 type HelloProps = {
-    user: string, 
-    password: string
+    onLogin?: any,
+    onLogout?: any,
 }
 
 type HelloState = {
 
 }
 
-class Hello extends React.Component<HelloState> {
+class Hello extends React.Component<HelloProps, HelloState> {
     constructor(props: HelloProps) {
         super(props)
-        this.state = {}
-    }
-
-    onLogin() {
-
-    }
-
-    onLogout() {
-
     }
     
     render() {
+        const {onLogin, onLogout} = this.props;
         return (
             <div>
                 <h1>Hello</h1>
-                <button onClick={this.onLogin.bind(this)}>Login</button>
-                <button onClick={this.onLogout.bind(this)}>Logout</button>
+                <button onClick={onLogin}>Login</button>
+                <button onClick={onLogout}>Logout</button>
             </div>
         )
     }
 }
 
 const mapStateToProps = (state: HelloState, ownProps: HelloProps) => {
-    console.log(state, ownProps);
     return {
         value: state
     }
 }
 const mapDispatchToProps = (dispatch: Dispatch, ownProps: HelloProps) => {
     return {
-        login: () => {
-            dispatch(login({user: 'str', password: "123"}))
+        onLogin: () => {
+            dispatch(login({user: 'admin', password: '123'}))
+        },
+        onLogout: () => {
+            dispatch(logout())
         }
     }
 }
--- app/src/store.tsx
+++ app/src/store.tsx
@@ -1,6 +1,6 @@
-import {createStore} from 'redux';
+import {createStore } from 'redux';
 import { LogAction } from './actions'
 import logReducer from './reducers'
-import { StoreState } from './types'; 
+import { StoreState } from './types';
 
 export const store = createStore<StoreState, LogAction, {}, {}>(logReducer, {user: 'admin', password: "123"})
