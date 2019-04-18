9d2b594db2c427103d6b311bac6e0a54f5378a38
celeking <celekingm_cn@outlook.com>
2019-04-18 23:46:25 +0800

    file

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,18 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom';
-import Router from './router';
-
-import { Provider } from 'react-redux';
-
 import Hello from './components/Hello'
-import {store} from './store';
 
 const rootElement = document.getElementById('root')
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Router/>
-    </BrowserRouter>,
+    <Hello />,
     rootElement
 )
